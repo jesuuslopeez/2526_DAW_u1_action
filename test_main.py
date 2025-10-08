7d666448d94da4f9903b6bffc018f63fc391c056
Jesús López Pérez <jlopezp@iesrafaelalberti.es>
2025-10-08 19:34:22 +0200

    Replace saludo test with saludar and sumar tests

--- test_main.py
+++ test_main.py
@@ -1,4 +1,13 @@
-from main import saludo
+from main import saludar, sumar
 
-def test_saludo():
-    assert saludo("Mundo") == "Hola, Mundo!"
+def test_saludar():
+    assert saludar("Jesus") == "Hola, Jesus!"
+    assert saludar("Lopez") == "Hola, Lopez!"
+    assert saludar("Perez") == "Hola, Perez!"
+
+def test_sumar():
+    assert sumar(1, 1) == 2
+    assert sumar(2, 2) == 4
+    assert sumar(3, 3) == 6
+    assert sumar(4, 4) == 8
+    assert sumar(5, 5) == 10
